dce694b4658ad3a840fb8055739eb4202617fc3e
Thanathorn Satayamana <ssatayamana@gmail.com>
2024-08-10 22:53:36 +0700

    initialize project

---
+++ sourcerer1/src/main/java/com/sourcerer/sourcerer1/Sourcerer1Application.java
@@ -0,0 +1,14 @@
+package com.sourcerer.sourcerer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sourcerer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sourcerer1Application.class, args);
+		System.out.println("Server is Running");
+	}
+
+}
---
+++ sourcerer1/src/test/java/com/sourcerer/sourcerer1/Sourcerer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcerer.sourcerer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sourcerer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
