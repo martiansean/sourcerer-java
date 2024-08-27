824076302ebac77ba6e0e20191b4c6a23bc1d9e2
Thanathorn Satayamana <ssatayamana@gmail.com>
2024-08-27 11:58:32 +0700

    add sentiment analyzer

---
+++ sourcerer1/src/main/java/com/sourcerer/sourcerer1/news/SentimentAnalyzer.java
@@ -0,0 +1,14 @@
+package com.sourcerer.sourcerer1.news;
+
+
+public class SentimentAnalyzer {
+
+    public static void main(String[] args) {
+        //Generate prompt
+        //Either use REST API to OPEN_AI or Claude
+
+        //Using gPRC to python service??
+
+
+    }
+}
